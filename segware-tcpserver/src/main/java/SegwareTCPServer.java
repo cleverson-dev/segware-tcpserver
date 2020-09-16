9f2560bea6da0e358f966f81121ca8af78c3d6a6
cleverson <cleverson.dev@gmail.com>
2020-09-16 11:39:27 -0300

    string codec addition

--- segware-tcpserver/src/main/java/SegwareTCPServer.java
+++ segware-tcpserver/src/main/java/SegwareTCPServer.java
@@ -1,8 +1,11 @@
 import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
 
 public class SegwareTCPServer {
 
@@ -11,7 +14,10 @@ public class SegwareTCPServer {
     public static void main( String[] args ) throws IOException {
         IoAcceptor acceptor = new NioSocketAcceptor();
 
-        acceptor.setHandler(  new ServerHandler() );
-        acceptor.bind( new InetSocketAddress(PORT) );
+        acceptor.getFilterChain().addLast("string-codec", new ProtocolCodecFilter(
+                new TextLineCodecFactory(Charset.forName("UTF-8"))));
+
+        acceptor.setHandler(new ServerHandler());
+        acceptor.bind(new InetSocketAddress(PORT));
     }
 }
