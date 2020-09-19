37843f6494c8a865e7fd578930abc71117bac7c8
cleverson <cleverson.dev@gmail.com>
2020-09-19 14:31:19 -0300

    hibernate setup

--- segware-tcpserver/src/main/java/com/segware/SegwareTCPServer.java
+++ segware-tcpserver/src/main/java/com/segware/SegwareTCPServer.java
@@ -17,11 +17,6 @@ public class SegwareTCPServer {
     private static final int PORT = 5050;
 
     public static void main( String[] args ) throws IOException, ClassNotFoundException {
-        setupMina();
-        setupH2();
-    }
-
-    private static void setupMina() throws IOException {
         IoAcceptor acceptor = new NioSocketAcceptor();
 
         acceptor.getFilterChain().addLast("logger", new LoggingFilter());
@@ -29,22 +24,5 @@ public class SegwareTCPServer {
 
         acceptor.setHandler(new ServerHandler());
         acceptor.bind(new InetSocketAddress(PORT));
-    }
-
-    // TODO: export information to properties file
-    private static void setupH2() throws ClassNotFoundException {
-        final String JDBC_DRIVER = "org.h2.Driver";
-        final String DB_URL = "jdbc:h2:~/message-db";
-
-        final String USER = "admin";
-        final String PASS = "pass";
-
-        Class.forName(JDBC_DRIVER);
-
-        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
-            System.out.println("Successful connection!");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
     }
 }
